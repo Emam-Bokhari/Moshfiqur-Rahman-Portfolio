b01105c29a62d454f717c42c5c1a3a62dd9f4373
Moshfiqur Rahman <studentemam@gmail.com>
2025-03-20 20:23:18 +0600

    feat(api): add server function to fetch all skills

---
+++ src/services/Skill/index.ts
@@ -0,0 +1,11 @@
+"use server";
+
+export const getAllSkills = async () => {
+    try {
+        const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_API}/skills`);
+        const data = await res.json();
+        return data;
+    } catch (error: any) {
+        throw new Error(error);
+    }
+};
