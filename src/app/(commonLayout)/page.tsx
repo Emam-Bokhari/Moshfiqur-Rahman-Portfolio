721bdfa67bc51015b4a417c0d64f6ba435b1a774
Moshfiqur Rahman <studentemam@gmail.com>
2025-03-20 20:16:32 +0600

    feat: add banner ui

---
+++ src/app/(commonLayout)/page.tsx
@@ -0,0 +1,13 @@
+import Banner from "@/components/modules/Home/Banner";
+import { Pattern } from "@/components/modules/Home/Pattern";
+
+export default async function HomePage() {
+  return (
+    <div className="lg:max-w-screen-2xl p-4 mx-auto ">
+      <Banner />
+      <Pattern />
+      {/* <Skills />
+      <FeaturedProject featuredProject={featuredProject} /> */}
+    </div>
+  );
+}
