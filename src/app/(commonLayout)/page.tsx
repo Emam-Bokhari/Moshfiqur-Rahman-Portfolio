a4cba16288a6c17aac695b4dcabe1639790c5e4b
Moshfiqur Rahman <studentemam@gmail.com>
2025-03-20 20:47:51 +0600

    feat: implement skill section and functionality

--- src/app/(commonLayout)/page.tsx
+++ src/app/(commonLayout)/page.tsx
@@ -1,13 +1,14 @@
 import Banner from "@/components/modules/Home/Banner";
 import { Pattern } from "@/components/modules/Home/Pattern";
+import Skills from "@/components/modules/Home/Skills";
 
 export default async function HomePage() {
   return (
     <div className="lg:max-w-screen-2xl p-4 mx-auto ">
       <Banner />
       <Pattern />
-      {/* <Skills />
-      <FeaturedProject featuredProject={featuredProject} /> */}
+      <Skills />
+      {/* <FeaturedProject featuredProject={featuredProject} /> */}
     </div>
   );
 }
