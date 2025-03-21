86cc59ba1debdbd0e5291fbad5ebba7be0f9e7ce
Moshfiqur Rahman <studentemam@gmail.com>
2025-03-21 10:18:19 +0600

    feat: add server action for submitting contact data

---
+++ src/services/Contact/index.ts
@@ -0,0 +1,20 @@
+"use server"
+
+export const addContact = async (contactData: any) => {
+    try {
+        const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_API}/contacts`, {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+
+            },
+            body: JSON.stringify(contactData),
+        });
+
+        const data = await res.json();
+
+        return data;
+    } catch (error: any) {
+        throw new Error(error);
+    }
+};
